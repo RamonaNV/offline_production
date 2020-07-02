e9d0610dd088647f6b03394263f2ae1fefd2837b
Vishal Mehta <vishalm@nvidia.com>
2020-07-02 15:00:37 +0200

    add printf on build options

--- clsim/private/opencl/I3CLSimStepToPhotonConverterOpenCL.cxx
+++ clsim/private/opencl/I3CLSimStepToPhotonConverterOpenCL.cxx
@@ -663,6 +663,7 @@ void I3CLSimStepToPhotonConverterOpenCL::SetupQueueAndKernel(const cl::Platform 
         BuildOptions += "-DNO_FLASHER ";
     }
 
+    std::cout << BuildOptions << std::endl;
     cl::Program program;
     try {
         // build the program
