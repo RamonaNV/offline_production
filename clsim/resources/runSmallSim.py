fd60b445afcb5bb7093862158046964077be9fba
Hendrik Schwanekamp <hschwanekamp@nvidia.com>
2020-12-14 17:41:51 +0100

    add repeat option to runSmallSim script

--- clsim/resources/runSmallSim.py
+++ clsim/resources/runSmallSim.py
@@ -43,120 +43,125 @@ parser.add_argument('--use-gpus', default=True, action='store_true')
 parser.add_argument('--oversize', default=1, type=int)
 parser.add_argument('--energy', default=1e3, type=float)
 parser.add_argument('-o', '--output-file', default=None)
+parser.add_argument('-r', '--repeat',default=1, type=int)
+
 
 args = parser.parse_args()
 
-DetectorParams = clsim.traysegments.common.setupDetector(
-    GCDFile=args.gcd_file,
-    DOMOversizeFactor=args.oversize,
-    HoleIceParameterization=expandvars("$I3_SRC/ice-models/resources/models/angsens/as.nominal"),
-    IceModelLocation=expandvars("$I3_SRC/ice-models/resources/models/spice_3.2.2-for_clsim")
-)
-icetray.logging.set_level_for_unit('ppc', 'WARN')
+for i in range(0,args.repeat):
 
-rng = phys_services.I3GSLRandomService(0)
+	DetectorParams = clsim.traysegments.common.setupDetector(
+		GCDFile=args.gcd_file,
+		DOMOversizeFactor=args.oversize,
+		HoleIceParameterization=expandvars("$I3_SRC/ice-models/resources/models/angsens/as.nominal"),
+		IceModelLocation=expandvars("$I3_SRC/ice-models/resources/models/spice_3.2.2-for_clsim")
+	)
+	icetray.logging.set_level_for_unit('ppc', 'WARN')
 
-from icecube.ppc import MakeCLSimPropagator
- 
+	rng = phys_services.I3GSLRandomService(0)
 
-clsimer = clsim.traysegments.common.setupPropagators(rng, DetectorParams, UseCPUs=not args.use_gpus)[0]
-
- 
-#pprint('---> clsim granularity %d, bunch size %d' % (clsimer.workgroupSize, clsimer.maxNumWorkitems))
-
-try:
-    from math import gcd
-except ImportError:
-    from fractions import gcd
-lcm = lambda a,b: a*b/gcd(a,b)
-granularity = int( clsimer.workgroupSize)
-maxBunchSize = clsimer.maxNumWorkitems 
-maxBunchSize -= (maxBunchSize % granularity)
-#pprint('---> common granularity %d, bunch size %d' % (granularity, maxBunchSize))
-
-stepGenerator = clsim.I3CLSimLightSourceToStepConverterAsync()
-
-stepGenerator.SetLightSourceParameterizationSeries(DetectorParams['ParameterizationList'])
-stepGenerator.SetMediumProperties(DetectorParams['MediumProperties'])
-stepGenerator.SetRandomService(rng)
-stepGenerator.SetWlenBias(DetectorParams['WavelengthGenerationBias'])
-stepGenerator.SetMaxBunchSize(maxBunchSize)
-stepGenerator.SetBunchSizeGranularity(granularity)
-stepGenerator.Initialize()
-
-p = dataclasses.I3Particle()
-p.type = p.EMinus
-p.energy = args.energy
-p.time = 0
-p.pos = dataclasses.I3Position(0,0,-400)
-p.dir = dataclasses.I3Direction(0,0)
-
-for i in range(1):
-	stepGenerator.EnqueueLightSource(clsim.I3CLSimLightSource(p), 0)
-stepGenerator.EnqueueBarrier()
-
-from collections import defaultdict
-photons = defaultdict(clsim.I3CLSimPhotonSeries)
-
-i = 0
-while True:
-	steps, markers, particleHistories, barrierWasReset = stepGenerator.GetConversionResultWithBarrierInfoAndMarkers()
-
-	#print('---> sending %d photons in bunch %d' % (sum((s.num for s in steps)), i))
+	from icecube.ppc import MakeCLSimPropagator
 	
 
- 
-	clsimer.EnqueueSteps(steps, i)
-	i += 1
+	clsimer = clsim.traysegments.common.setupPropagators(rng, DetectorParams, UseCPUs=not args.use_gpus)[0]
+
 	
-	 
-	result_clsim =  clsimer.GetConversionResult()
+	#pprint('---> clsim granularity %d, bunch size %d' % (clsimer.workgroupSize, clsimer.maxNumWorkitems))
+
+	try:
+		from math import gcd
+	except ImportError:
+		from fractions import gcd
+	lcm = lambda a,b: a*b/gcd(a,b)
+	granularity = int( clsimer.workgroupSize)
+	maxBunchSize = clsimer.maxNumWorkitems 
+	maxBunchSize -= (maxBunchSize % granularity)
+	#pprint('---> common granularity %d, bunch size %d' % (granularity, maxBunchSize))
+
+	stepGenerator = clsim.I3CLSimLightSourceToStepConverterAsync()
+
+	stepGenerator.SetLightSourceParameterizationSeries(DetectorParams['ParameterizationList'])
+	stepGenerator.SetMediumProperties(DetectorParams['MediumProperties'])
+	stepGenerator.SetRandomService(rng)
+	stepGenerator.SetWlenBias(DetectorParams['WavelengthGenerationBias'])
+	stepGenerator.SetMaxBunchSize(maxBunchSize)
+	stepGenerator.SetBunchSizeGranularity(granularity)
+	stepGenerator.Initialize()
+
+	p = dataclasses.I3Particle()
+	p.type = p.EMinus
+	p.energy = args.energy
+	p.time = 0
+	p.pos = dataclasses.I3Position(0,0,-400)
+	p.dir = dataclasses.I3Direction(0,0)
+
+	for i in range(1):
+		stepGenerator.EnqueueLightSource(clsim.I3CLSimLightSource(p), 0)
+	stepGenerator.EnqueueBarrier()
+
+	from collections import defaultdict
+	photons = defaultdict(clsim.I3CLSimPhotonSeries)
+
+	i = 0
+	while True:
+		steps, markers, particleHistories, barrierWasReset = stepGenerator.GetConversionResultWithBarrierInfoAndMarkers()
+
+		#print('---> sending %d photons in bunch %d' % (sum((s.num for s in steps)), i))
+		
+
 	
- 
-	photons['clsim'].extend(result_clsim.photons)
-
-	 
-	n_clsim = len(result_clsim.photons)
-#p	print('---> got  clsim: %d photons in bunch %d' % ( n_clsim, result_clsim.identifier))
+		clsimer.EnqueueSteps(steps, i)
+		i += 1
+		
+		
+		result_clsim =  clsimer.GetConversionResult()
+		
 	
-	if barrierWasReset:
-		break
+		photons['clsim'].extend(result_clsim.photons)
+
+		
+		n_clsim = len(result_clsim.photons)
+	#p	print('---> got  clsim: %d photons in bunch %d' % ( n_clsim, result_clsim.identifier))
+		
+		if barrierWasReset:
+			break
 
 
-n_clsim = len(photons['clsim'])
-#pprint('total unweighted clsim: %d photons  ' %   n_clsim )
+	n_clsim = len(photons['clsim'])
+	#pprint('total unweighted clsim: %d photons  ' %   n_clsim )
 
-n_clsim = sum([p.weight for p in photons['clsim']])
+	n_clsim = sum([p.weight for p in photons['clsim']])
 
 
-#pprint('total weighted clsim: %d photons  ' %   n_clsim )
+	#pprint('total weighted clsim: %d photons  ' %   n_clsim )
 
- 
- 
-# Down-convert to MCPEs. If the Cherenkov photon generator is configured
-# correctly, this should remove any constant or wavelength-dependent scale
-# factors that may be present in the raw number of detected photons.
-hitter = clsim.I3CLSimPhotonToMCPEConverterForDOMs(
-    rng,
-    DetectorParams['WavelengthAcceptance'],
-    DetectorParams['AngularAcceptance']
-)
-def generate_mcpe(photons):
-	for clsim_photon in photons:
-		p = simclasses.I3CompressedPhoton()
-		p.dir = clsim_photon.dir
-		p.pos = clsim_photon.pos
-		p.time = clsim_photon.time
-		p.wavelength = clsim_photon.wavelength
-		p.weight = clsim_photon.weight
-		key = dataclasses.ModuleKey(clsim_photon.stringID, clsim_photon.omID)
-		hit = hitter.Convert(key, p)
-		if hit:
-			yield hit[1]
+	
+	
+	# Down-convert to MCPEs. If the Cherenkov photon generator is configured
+	# correctly, this should remove any constant or wavelength-dependent scale
+	# factors that may be present in the raw number of detected photons.
+	hitter = clsim.I3CLSimPhotonToMCPEConverterForDOMs(
+		rng,
+		DetectorParams['WavelengthAcceptance'],
+		DetectorParams['AngularAcceptance']
+	)
+	def generate_mcpe(photons):
+		for clsim_photon in photons:
+			p = simclasses.I3CompressedPhoton()
+			p.dir = clsim_photon.dir
+			p.pos = clsim_photon.pos
+			p.time = clsim_photon.time
+			p.wavelength = clsim_photon.wavelength
+			p.weight = clsim_photon.weight
+			key = dataclasses.ModuleKey(clsim_photon.stringID, clsim_photon.omID)
+			hit = hitter.Convert(key, p)
+			if hit:
+				yield hit[1]
 
- 
-n_clsim = len(list(generate_mcpe(photons['clsim'])))
-print('total clsim: %d' % ( n_clsim ))
+	
+	n_clsim = len(list(generate_mcpe(photons['clsim'])))
+	print('total clsim: %d' % ( n_clsim ))
 
-if args.output_file:
-    numpy.savez(args.output_file, **photons)
+	if args.output_file:
+		numpy.savez(args.output_file+i, **photons)
+
